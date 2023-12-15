1f0e3a222ee9e7f41a0899ce77f2fd89377024e5
slubendo <stephane.lubendo@gmail.com>
2023-12-15 15:30:01 -0800

    postQuery and post page

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,4 +1,5 @@
 import SinglePost from "@/components/single-post"
+import { postQuery } from "@/db/queries/post-query"
 
 import { notFound } from "next/navigation"
 
@@ -6,7 +7,7 @@ import { notFound } from "next/navigation"
 export default async function Post({ params }: { params: { id: string } }) {
 
   // fake post
-  const post = {
+  const fakePost = {
     id: "1",
     user: {
       id: "1",
@@ -16,6 +17,8 @@ export default async function Post({ params }: { params: { id: string } }) {
     createdAt: "2021-09-01T12:00:00.000Z",
     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
   }
+  
+  let post = await postQuery(parseInt(params.id))
 
   if (!post) {
     notFound()
@@ -24,7 +27,7 @@ export default async function Post({ params }: { params: { id: string } }) {
 
   return (
     <div className="flex flex-col divide-y">
-      <SinglePost post={post} />
+      <SinglePost post={fakePost} />
     </div>
   )
 }
---
+++ src/db/queries/post-query.ts
@@ -0,0 +1,23 @@
+import { eq } from "drizzle-orm"
+import { posts } from "../schema/post";
+import { feedQuery } from "./feed-query";
+
+
+export async function postQuery(id: number) {
+    try {
+        const singlePost = await feedQuery
+            .where(eq(posts.id, id))
+            .then((res) => res[0]);
+
+        if (singlePost) {
+            return {
+                "success": "single post",
+                singlePost: singlePost
+            }
+        } else {
+            return { "error": "cannot find post" }
+        }
+    } catch (err) {
+        return { "error": "cannot find post" }
+    }
+}
